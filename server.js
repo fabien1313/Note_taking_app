65750ff0c3380873116e2a15d21fe3f509f4fe06
fabien moreno <fabienmoreno1331@yahoo.com>
2023-01-17 01:33:36 -0600

    routes working

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const uuid = require('uuid');
+const nodemon = require('nodemon');
 
 const app = express();
 
@@ -11,23 +12,35 @@ const PORT = process.env.PORT || 3001;
 
 
 app.use(express.json()); // middleware
+app.use(express.static(path.join(__dirname, '/public'))); //middleware because it couldnt find css sheets.
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'));
+    res.sendFile(path.join(__dirname, '/public/index.html')); //Route  to index.html
 });
 
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/notes.html'));
+    res.sendFile(path.join(__dirname, '/public/notes.html')); //Route to notes.html
 });
 
 app.get('/api/notes', (req, res) => {
-    let notes = fs.readFileSync(path.join(__dirname, '/db/db.json'));
+    let notes = fs.readFileSync(path.join(__dirname, '/db/db.json')); //retrieving the db(displaying as json obj)
     res.json(JSON.parse(notes));
+});
 
-})
+app.post('/api/notes', (req, res) => {
+    let notes = JSON.parse(fs.readFileSync(path.join(__dirname, '/db/db.json'), 'utf8'));
+    let addNote = { id: uuid.v4(), ...req.body };
+    notes.push(addNote);
+    fs.writeFileSync(path.join(__dirname, '/db/db.json'), JSON.stringify(notes));
+    res.json(addNote);
+});
 
-
-
+app.delete('/api/notes/:id', (req, res) => {
+    let notes = JSON.parse(fs.readFileSync(path.join(__dirname, '/db/db.json'), 'utf8'));
+    notes = notes.filter(note => note.id !== req.params.id);
+    fs.writeFileSync(path.join(__dirname, '/db/db.json'), JSON.stringify(notes));
+    res.json({ response: `Note ID: ${req.params.id} has been deleted from log.` });
+});
 
 app.listen(PORT, () => {
     console.log(`App is listening at http://localhost:${PORT}`);
