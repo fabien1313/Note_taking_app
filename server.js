8f1ae1dce570c9971f09a1d35090589c7ed754f3
fabien moreno <fabienmoreno1331@yahoo.com>
2023-01-17 01:39:29 -0600

    functionality test for all routes complete

--- server.js
+++ server.js
@@ -10,23 +10,24 @@ const app = express(); //invoking express
 const PORT = process.env.PORT || 3001; //Learned from Mosh.
 
 
-
-app.use(express.json()); // middleware
+// middleware
+app.use(express.json()); 
 app.use(express.static(path.join(__dirname, '/public'))); //middleware because it couldnt find css sheets.
 
+
+//GETS
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html')); //Route  to index.html
 });
-
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html')); //Route to notes.html
 });
-
 app.get('/api/notes', (req, res) => {
     let notes = fs.readFileSync(path.join(__dirname, '/db/db.json')); //retrieving the db(displaying as json obj)
     res.json(JSON.parse(notes));
 });
 
+//POSTS
 app.post('/api/notes', (req, res) => {
     let notes = JSON.parse(fs.readFileSync(path.join(__dirname, '/db/db.json'), 'utf8'));
     let addNote = { id: uuid.v4(), ...req.body }; //assigning random ID.
@@ -35,6 +36,7 @@ app.post('/api/notes', (req, res) => {
     res.json(addNote);
 });
 
+//deletes note
 app.delete('/api/notes/:id', (req, res) => {
     let notes = JSON.parse(fs.readFileSync(path.join(__dirname, '/db/db.json'), 'utf8'));
     notes = notes.filter(note => note.id !== req.params.id);
@@ -42,6 +44,8 @@ app.delete('/api/notes/:id', (req, res) => {
     res.json({ response: `Note ID: ${req.params.id} has been deleted from log.` });
 });
 
+
+//Listen
 app.listen(PORT, () => {
-    console.log(`App is listening at http://localhost:${PORT}`);
+    console.log(`App is listening at http://localhost:${PORT}`); //listening for port
 });
